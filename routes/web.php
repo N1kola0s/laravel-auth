b6d99dbd02383c88cd7c4a0e96d0fc6a6ba238f3
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-06-21 22:54:38 +0200

    aggiunto percorso extends layouts.admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 /* Route::get('/', function () {
     return view('welcome');
-});
- */
+}); */
+
 
 Auth::routes();
 
@@ -24,6 +25,7 @@ Auth::routes();
 Route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->group(function(){
     Route::get('/', 'HomeController@index')->name('home');
 });
+
 
 Route::get('{any?}', function(){
     return view('guest.home');
